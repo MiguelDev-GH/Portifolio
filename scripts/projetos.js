6b11ab458f89506a6e133a2ace0695d5243dcfa6
Miguel Chagas Maciel <miguelchagasmaciel@gmail.com>
2025-08-30 18:01:03 -0300

    Mudado as cores do site e alguns detalhes

--- scripts/projetos.js
+++ scripts/projetos.js
@@ -1,10 +1,25 @@
 const setaDireita = document.getElementById("ProjDireita");
 const setaEsquerda = document.getElementById("ProjEsquerda");
 
+const contadorDiv = document.getElementById(`contadorProjDiv`);
+
 const projQntd = 2;
 let projAtual = 1;
+
 function atualizarDisplayProjetos() {
+    contadorDiv.innerHTML = ""
     for (let i = 1; i <= projQntd; i++) {
+
+        let contElem = document.createElement("div");
+        contElem.classList.add("contadorprojeto");
+        contElem.id = `cp${i}`;
+
+        if(i === projAtual){
+            contElem.classList.add("contadorativo");
+        }
+
+        contadorDiv.appendChild(contElem);
+
         const projetoEl = document.getElementById(`projeto${i}`);
         if (projetoEl) {
             projetoEl.style.display = (i === projAtual) ? "flex" : "none";
